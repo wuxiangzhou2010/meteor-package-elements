43618e9880a9ca2d053fdf4c6ef92c600eb919c2
wuxiangzhou2010 <wuxiangzhou2010@gmail.com>
2019-03-14 11:19:12 +0800

    for ethereum:elements@0.0.1

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'ethereum:elements',
+  name: 'mubiale:elements',
   summary: 'Basic elements for Dapps',
-  version: '0.7.18',
-  git: 'http://github.com/ethereum/meteor-package-elements'
+  version: '0.0.1',
+  git: 'http://github.com/wuxiangzhou2010/meteor-package-elements'
 });
 
 Package.onUse(function(api) {
@@ -18,14 +18,14 @@ Package.onUse(function(api) {
   api.use('3stack:bignumber@2.0.0', 'client');
   // api.use('underscorestring:underscore.string@3.1.1', 'client');
 
-  api.use('ethereum:web3@0.15.2', 'client');
-  api.use('ethereum:tools@0.4.0', 'client');
+  api.use('mubiale:web3@0.0.2', 'client');
+  api.use('mubiale:tools@0.0.1', 'client');
   api.use('frozeman:animation-helper@0.2.5', 'client');
   api.use('frozeman:storage@0.1.8', 'client');
   api.use('frozeman:template-var@1.2.2', 'client');
 
   // provide packages for the app developer as well
-  api.imply(['frozeman:template-var','ethereum:tools'], 'client');
+  api.imply(['frozeman:template-var','mubiale:tools'], 'client');
 
 
   api.export(['EthElements'], 'client');
